7bb014c3332661404e91027611677cece776d0ff
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2018-10-23 00:18:45 +0530

    comment edit delete update

--- routes/comments.js
+++ routes/comments.js
@@ -62,6 +62,40 @@ router.post("/",isLoggedIn ,function(req, res){
         }
     });
 });
+
+router.get("/:comment_id/edit",function(req,res){
+     Comment.findById(req.params.comment_id,function(err,comment){
+         if(err){
+             console.log(err);
+             res.redirect("back");
+         } else {
+            res.render("comments/edit",{campground_id: req.params.id, comment: comment} );
+        }
+     });
+});
+
+router.put("/:comment_id",function(req,res){
+    Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,function(err,updatedComment){
+        if(err){
+            console.log(err);
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/"+req.params.id);
+        }
+    });
+});
+
+router.delete("/:comment_id",function(req,res){
+    Comment.findByIdAndDelete(req.params.comment_id,function(err){
+        if(err){
+            cosole.log(err);
+            res.redirect("back");
+        } else {
+            res.redirect("back");
+        }
+    })
+})
+
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
         return next();

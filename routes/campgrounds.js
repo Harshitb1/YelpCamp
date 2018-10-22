684da6797a29bfd51459c4ac1acebe2602abfefc
Harshitb1 <36952154+Harshitb1@users.noreply.github.com>
2018-10-22 23:34:23 +0530

    authorization

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -54,17 +54,19 @@ router.get("/:id",function(req,res){
     });
 }); 
 
-router.get("/:id/edit",function(req,res){
-    Campground.findById(req.params.id,function(err,campground){
-        if(err){
-            res.redirect("/campgrounds")
-        } else {
-            res.render("campgrounds/edit",{campground:campground});
-        }
-    });
+router.get("/:id/edit",checkCampgroundOwnership,function(req,res){
+        
+        Campground.findById(req.params.id,function(err,campground){
+            if(err){
+                res.redirect("/campgrounds")
+            } else {
+                res.render("campgrounds/edit",{campground:campground});
+           
+            }
+        });
 });
 
-router.put("/:id",function(req,res){
+router.put("/:id",checkCampgroundOwnership,function(req,res){
     Campground.findByIdAndUpdate(req.params.id,req.body.campground,function(err,updatedCampground){
         if(err){
             res.redirect("/campgrounds");
@@ -74,7 +76,7 @@ router.put("/:id",function(req,res){
     });
 });
 
-router.delete("/:id",function(req,res){
+router.delete("/:id",checkCampgroundOwnership  ,function(req,res){
     Campground.findByIdAndRemove(req.params.id,function(err){
         if(err){
             res.redirect("/campgrounds");
@@ -93,5 +95,28 @@ function isLoggedIn(req,res,next){
     }
 }
 
+function checkCampgroundOwnership(req,res,next ){
+    if(req.isAuthenticated()){
+        
+        Campground.findById(req.params.id,function(err,campground){
+            if(err){
+                res.redirect("/campgrounds")
+            } else {
+                // console.log(campground.author.id);
+                // console.log(req.user._id);
+                if(campground.author.id.equals(req.user._id )){
+
+                    next();
+                }else{
+                    res.redirect("back");
+                }
+            }
+        });
+
+    } else {
+        res.redirect("back  ");
+    }
+   
+}
 
 module.exports =router;
